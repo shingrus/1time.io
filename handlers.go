bf8ecbf31500ebd90d70aaf5b7980a24cc94d6bc
Igor Yermako <shingrus@gmail.com>
2026-03-16 22:23:56 +0000

    removed outdated plain http form api

--- handlers.go
+++ handlers.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"crypto/sha256"
 	"encoding/json"
-	"fmt"
-	"html/template"
+	//"html/template"
 	"log"
 	"net/http"
-	"strconv"
 	"time"
-
-	"github.com/Luzifer/go-openssl"
 )
 
 type StoredMessage struct {
@@ -19,159 +14,159 @@ type StoredMessage struct {
 	HashedKey string `json:"hashedKey"`
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("templates/form.html")
-	if err != nil {
-		log.Panic(err)
-	}
-	t.Execute(w, "")
+//func indexHandler(w http.ResponseWriter, r *http.Request) {
+//	t, err := template.ParseFiles("templates/form.html")
+//	if err != nil {
+//		log.Panic(err)
+//	}
+//	t.Execute(w, "")
+//
+//}
 
-}
+//func saveSecretHandler(w http.ResponseWriter, r *http.Request) {
+//
+//	if err := r.ParseForm(); err != nil {
+//		log.Println(err)
+//	}
+//	secretMessage := r.Form.Get(secretMessageFieldName)
+//
+//	if secretMessage == "" {
+//		http.Redirect(w, r, "/?empty", http.StatusSeeOther)
+//		return
+//	}
+//	if len(secretMessage) > secretMessageMaxLen {
+//		secretMessage = secretMessage[0:secretMessageMaxLen]
+//	}
+//
+//	secretkey := r.Form.Get(secretKeyFieldName)
+//	randKey := RandStringBytesMaskImprSrc(randKeyLen)
+//
+//	var duration time.Duration
+//	if d := r.Form.Get("duration"); d != "" {
+//		t, _ := strconv.Atoi(d)
+//		duration = time.Second * time.Duration(t)
+//	}
+//	if duration <= 0 || duration > 86400 {
+//		duration = defaultDuration * time.Second
+//	}
+//
+//	encryptKey := secretkey + randKey
+//
+//	var newMessage StoredMessage
+//
+//	// encrypt
+//
+//	newkey := get32key(encryptKey)
+//	newMessage.Message = encrypt([]byte(newkey), encryptKey+secretMessage)
+//	newMessage.Encrypted = true
+//
+//	value, _ := json.Marshal(newMessage)
+//	// store to storage
+//
+//	storeKey, err := saveToStorage(value, time.Second*duration)
+//	if err != nil {
+//		showError(500, "Storage error", w)
+//		return
+//	}
+//
+//	t, err := template.ParseFiles("templates/result.html")
+//	if err != nil {
+//		log.Panic(err)
+//	}
+//	linkKey := randKey + storeKey
+//
+//	data := struct {
+//		OneTimeLink string
+//		SecretKey   string
+//	}{linkKey, secretkey}
+//	t.Execute(w, data)
+//
+//}
 
-func saveSecretHandler(w http.ResponseWriter, r *http.Request) {
-
-	if err := r.ParseForm(); err != nil {
-		log.Println(err)
-	}
-	secretMessage := r.Form.Get(secretMessageFieldName)
-
-	if secretMessage == "" {
-		http.Redirect(w, r, "/?empty", http.StatusSeeOther)
-		return
-	}
-	if len(secretMessage) > secretMessageMaxLen {
-		secretMessage = secretMessage[0:secretMessageMaxLen]
-	}
-
-	secretkey := r.Form.Get(secretKeyFieldName)
-	randKey := RandStringBytesMaskImprSrc(randKeyLen)
-
-	var duration time.Duration
-	if d := r.Form.Get("duration"); d != "" {
-		t, _ := strconv.Atoi(d)
-		duration = time.Second * time.Duration(t)
-	}
-	if duration <= 0 || duration > 86400 {
-		duration = defaultDuration * time.Second
-	}
-
-	encryptKey := secretkey + randKey
-
-	var newMessage StoredMessage
-
-	// encrypt
-
-	newkey := get32key(encryptKey)
-	newMessage.Message = encrypt([]byte(newkey), encryptKey+secretMessage)
-	newMessage.Encrypted = true
-
-	value, _ := json.Marshal(newMessage)
-	// store to storage
-
-	storeKey, err := saveToStorage(value, time.Second*duration)
-	if err != nil {
-		showError(500, "Storage error", w)
-		return
-	}
-
-	t, err := template.ParseFiles("templates/result.html")
-	if err != nil {
-		log.Panic(err)
-	}
-	linkKey := randKey + storeKey
-
-	data := struct {
-		OneTimeLink string
-		SecretKey   string
-	}{linkKey, secretkey}
-	t.Execute(w, data)
-
-}
-
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	key := r.URL.Path[len("/view/"):]
-	if len(key) <= randKeyLen {
-		log.Printf("Invalid  secretKey: %v", key)
-		return
-	}
-	randKey := key[:randKeyLen]
-	storeKey := key[randKeyLen:]
-	val := getMessageFromStorage(storeKey)
-	if len(val) > 0 {
-
-		var storedMessage StoredMessage
-		err := json.Unmarshal([]byte(val), &storedMessage)
-
-		if err == nil {
-			if r.Method != "POST" {
-
-				if storedMessage.Encrypted {
-					if t, err := template.ParseFiles("templates/getkeyform.html"); err != nil {
-						log.Panic(err)
-					} else {
-
-						data := struct {
-							Key string
-						}{key}
-						t.Execute(w, data)
-						return
-					}
-				} else {
-					showMessage(key, storedMessage, w)
-					return
-				}
-
-			} else {
-				r.ParseForm()
-				secretKey := r.Form.Get(secretKeyFieldName)
-				encryptKey := secretKey + randKey
-
-				keylen := len(encryptKey)
-				if keylen == 0 {
-					http.Redirect(w, r, "/view/"+key, http.StatusSeeOther)
-					return
-				}
-
-				if storedMessage.Encrypted {
-					newKey := get32key(encryptKey)
-
-					secretMessage := decrypt([]byte(newKey), storedMessage.Message)
-					if len(secretMessage) > keylen && secretMessage[0:keylen] == encryptKey {
-						storedMessage.Message = secretMessage[keylen:]
-						showMessage(key, storedMessage, w)
-						return
-					} else {
-						http.Redirect(w, r, "/view/"+key, http.StatusSeeOther)
-						return
-					}
-				} else {
-					showMessage(secretKey, storedMessage, w)
-					return
-				}
-
-			}
-		} else {
-			log.Println(err)
-		}
-
-	}
-	http.Redirect(w, r, "/", http.StatusSeeOther)
-
-}
-
-func showMessage(key string, message StoredMessage, w http.ResponseWriter) {
-	if t, err := template.ParseFiles("templates/view.html"); err != nil {
-		log.Panic(err)
-	} else {
-		dropFromStorage(key)
-		t.Execute(w, message)
-	}
-}
-
-func showError(errorStatus int, text string, w http.ResponseWriter) {
-	w.WriteHeader(errorStatus)
-	w.Write([]byte(text))
-}
+//func viewHandler(w http.ResponseWriter, r *http.Request) {
+//	key := r.URL.Path[len("/view/"):]
+//	if len(key) <= randKeyLen {
+//		log.Printf("Invalid  secretKey: %v", key)
+//		return
+//	}
+//	randKey := key[:randKeyLen]
+//	storeKey := key[randKeyLen:]
+//	val := getMessageFromStorage(storeKey)
+//	if len(val) > 0 {
+//
+//		var storedMessage StoredMessage
+//		err := json.Unmarshal([]byte(val), &storedMessage)
+//
+//		if err == nil {
+//			if r.Method != "POST" {
+//
+//				if storedMessage.Encrypted {
+//					if t, err := template.ParseFiles("templates/getkeyform.html"); err != nil {
+//						log.Panic(err)
+//					} else {
+//
+//						data := struct {
+//							Key string
+//						}{key}
+//						t.Execute(w, data)
+//						return
+//					}
+//				} else {
+//					showMessage(key, storedMessage, w)
+//					return
+//				}
+//
+//			} else {
+//				r.ParseForm()
+//				secretKey := r.Form.Get(secretKeyFieldName)
+//				encryptKey := secretKey + randKey
+//
+//				keylen := len(encryptKey)
+//				if keylen == 0 {
+//					http.Redirect(w, r, "/view/"+key, http.StatusSeeOther)
+//					return
+//				}
+//
+//				if storedMessage.Encrypted {
+//					newKey := get32key(encryptKey)
+//
+//					secretMessage := decrypt([]byte(newKey), storedMessage.Message)
+//					if len(secretMessage) > keylen && secretMessage[0:keylen] == encryptKey {
+//						storedMessage.Message = secretMessage[keylen:]
+//						showMessage(key, storedMessage, w)
+//						return
+//					} else {
+//						http.Redirect(w, r, "/view/"+key, http.StatusSeeOther)
+//						return
+//					}
+//				} else {
+//					showMessage(secretKey, storedMessage, w)
+//					return
+//				}
+//
+//			}
+//		} else {
+//			log.Println(err)
+//		}
+//
+//	}
+//	http.Redirect(w, r, "/", http.StatusSeeOther)
+//
+//}
+//
+//func showMessage(key string, message StoredMessage, w http.ResponseWriter) {
+//	if t, err := template.ParseFiles("templates/view.html"); err != nil {
+//		log.Panic(err)
+//	} else {
+//		dropFromStorage(key)
+//		t.Execute(w, message)
+//	}
+//}
+//
+//func showError(errorStatus int, text string, w http.ResponseWriter) {
+//	w.WriteHeader(errorStatus)
+//	w.Write([]byte(text))
+//}
 
 /**
 Json Payload should
@@ -183,70 +178,70 @@ var payload struct {
 		Duration      int    `json:"duration"`
 	}
 */
-func apiUnsecSave(r *http.Request) (responseCode int, response []byte) {
-	responseCode = 200
-	jResponse := struct {
-		Status  string `json:"status"`
-		NewLink string `json:"newLink"`
-	}{
-		Status:  "error",
-		NewLink: "",
-	}
-	var payload struct {
-		SecretMessage string `json:"secretMessage"`
-		Duration      int    `json:"duration"`
-	}
-	if r.Method != "POST" { // unsecure for default logging etc
-		jResponse.Status = "Invalid request method."
-	} else {
-		dec := json.NewDecoder(r.Body)
-		if dec.More() {
-			err := dec.Decode(&payload)
-			if err == nil {
-				if len(payload.SecretMessage) > 0 {
-					// log.Printf("Got payload: %v\n", payload)
-
-					randKey := RandStringBytesMaskImprSrc(randKeyLen)
-
-					if payload.Duration <= 0 || payload.Duration > maxDuration {
-						payload.Duration = defaultDuration
-					}
-
-					o := openssl.New()
-
-					enc, err := o.EncryptString(randKey, payload.SecretMessage)
-					if err == nil {
-						newMessage := StoredMessage{
-							Encrypted: true,
-							Message:   string(enc),
-							HashedKey: fmt.Sprintf("%x", sha256.Sum256([]byte(randKey))),
-						}
-
-						log.Printf("paylaod -> storage: %v, Hashedkey: %v, Duration: %v\n", newMessage.Message, newMessage.HashedKey, payload.Duration)
-						valueToStore, _ := json.Marshal(newMessage)
-						storeKey, err := saveToStorage(valueToStore, time.Duration(payload.Duration)*time.Second)
-						if err == nil {
-							jResponse.NewLink = "/v/#" + randKey + storeKey
-							jResponse.Status = "ok"
-						} else {
-							log.Println(err)
-						}
-
-					} else {
-						log.Println(err)
-					}
-
-				}
-			} else {
-				log.Println(err)
-			}
-
-		}
-	}
-	response, _ = json.Marshal(jResponse)
-	return
-
-}
+//func apiUnsecSave(r *http.Request) (responseCode int, response []byte) {
+//	responseCode = 200
+//	jResponse := struct {
+//		Status  string `json:"status"`
+//		NewLink string `json:"newLink"`
+//	}{
+//		Status:  "error",
+//		NewLink: "",
+//	}
+//	var payload struct {
+//		SecretMessage string `json:"secretMessage"`
+//		Duration      int    `json:"duration"`
+//	}
+//	if r.Method != "POST" { // unsecure for default logging etc
+//		jResponse.Status = "Invalid request method."
+//	} else {
+//		dec := json.NewDecoder(r.Body)
+//		if dec.More() {
+//			err := dec.Decode(&payload)
+//			if err == nil {
+//				if len(payload.SecretMessage) > 0 {
+//					// log.Printf("Got payload: %v\n", payload)
+//
+//					randKey := RandStringBytesMaskImprSrc(randKeyLen)
+//
+//					if payload.Duration <= 0 || payload.Duration > maxDuration {
+//						payload.Duration = defaultDuration
+//					}
+//
+//					o := openssl.New()
+//
+//					enc, err := o.EncryptString(randKey, payload.SecretMessage)
+//					if err == nil {
+//						newMessage := StoredMessage{
+//							Encrypted: true,
+//							Message:   string(enc),
+//							HashedKey: fmt.Sprintf("%x", sha256.Sum256([]byte(randKey))),
+//						}
+//
+//						log.Printf("paylaod -> storage: %v, Hashedkey: %v, Duration: %v\n", newMessage.Message, newMessage.HashedKey, payload.Duration)
+//						valueToStore, _ := json.Marshal(newMessage)
+//						storeKey, err := saveToStorage(valueToStore, time.Duration(payload.Duration)*time.Second)
+//						if err == nil {
+//							jResponse.NewLink = "/v/#" + randKey + storeKey
+//							jResponse.Status = "ok"
+//						} else {
+//							log.Println(err)
+//						}
+//
+//					} else {
+//						log.Println(err)
+//					}
+//
+//				}
+//			} else {
+//				log.Println(err)
+//			}
+//
+//		}
+//	}
+//	response, _ = json.Marshal(jResponse)
+//	return
+//
+//}
 func apiSaveSecret(r *http.Request) (responseCode int, response []byte) {
 	responseCode = 200
 
@@ -300,7 +295,8 @@ func apiSaveSecret(r *http.Request) (responseCode int, response []byte) {
 	return
 }
 
-/**
+/*
+*
 Good to have this api for everyone
 
 func apiGetRandomPass(r *http.Request) (responseCode int, response []byte) {
@@ -372,8 +368,8 @@ func apiHandler(w http.ResponseWriter, r *http.Request) {
 	switch apiCall {
 	case "saveSecret":
 		responseCode, response = apiSaveSecret(r)
-	case "unsecSave":
-		responseCode, response = apiUnsecSave(r)
+	//case "unsecSave":
+	//	responseCode, response = apiUnsecSave(r)
 	case "get":
 		responseCode, response = apiGetMessage(r)
 	default:
--- main.go
+++ main.go
@@ -8,18 +8,18 @@ import (
 
 const defaultDuration = 86400 * 7 // keep for 1 week
 const maxDuration = 86400 * 30    // keep for 1 month
-const randKeyLen = 12
+//const randKeyLen = 12
 
-const secretMessageFieldName = "secretMessage"
-const secretKeyFieldName = "secretKey"
-const secretMessageMaxLen = 64 * 1024
+//const secretMessageFieldName = "secretMessage"
+//const secretKeyFieldName = "secretKey"
+//const secretMessageMaxLen = 64 * 1024
 
 var _, DEBUG = os.LookupEnv("DEBUG")
 
 func main() {
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/saveSecret", saveSecretHandler)
-	http.HandleFunc("/view/", viewHandler)
+	//http.HandleFunc("/", indexHandler)
+	//http.HandleFunc("/saveSecret", saveSecretHandler)
+	//http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/api/", apiHandler)
 
 	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
